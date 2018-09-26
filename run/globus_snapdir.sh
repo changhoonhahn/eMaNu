c5e004d7e31ed1abb8fadb1994163ae6152fb95c
ChangHoon Hahn <chh327@nyu.edu>
2018-09-26 00:14:19 -0700

    script for transfering files via globus

---
+++ run/globus_snapdir.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+# transfer snapdir_002 from Paco to Nersc using Globus
+source_ep='8ecb2e28-da42-11e5-976d-22000b9da45e'
+dest_ep='9d6d994a-6d04-11e5-ba46-22000b92c6ec'
+   
+nersc_dir="/global/cscratch1/sd/chahah/emanu/"
+
+echo "password:" 
+read -s pwd 
+
+#0.0eV 
+#for mneut in 0.06eV 0.10eV 0.15eV 0.6eV; do 
+#done 
+
+mneut=0.0eV
+for nreal in {1..10}; do 
+    dir_nersc=$nersc_dir$mneut"/"$nreal"/snapdir_004/"
+    dir_cca="/mnt/ceph/users/fvillaescusa/Neutrino_simulations/Sims_Dec16_2/"$mneut"/"$nreal"/snapdir_004/"
+
+    #if sshpass -p $pwd ssh edison '[ ! -d '$dir_nersc' ]'; then
+    echo "transfering ... "$dir_nersc
+    globus transfer $source_ep:$dir_cca $dest_ep:$dir_nersc --recursive
+    #fi
+done 
