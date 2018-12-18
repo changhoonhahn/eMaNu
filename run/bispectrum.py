7411449c2325c4f5a58cc6e6184315af26ef6a89
changhoonhahn <changhoonhahn@lbl.gov>
2018-12-17 23:56:43 -0500

    calculating bispectrum locally

--- run/bispectrum.py
+++ run/bispectrum.py
@@ -4,6 +4,7 @@ calculate bispectrum
 
 
 '''
+import os 
 import sys 
 import numpy as np 
 # -- pyspectrum -- 
@@ -13,30 +14,12 @@ from emanu import util as UT
 
 
 def haloBispectrum(mneut, nreal, nzbin, Lbox=1000., zspace=False, 
-        Ngrid=360, Nmax=40, Ncut=3, step=3, silent=True): 
+        Ngrid=360, Nmax=40, Ncut=3, step=3, silent=True, overwrite=False): 
     if zspace: raise NotImplementedError
     else: str_space = 'r' 
     fhalo = ''.join([UT.dat_dir(), 'halos/', 
         'groups.', str(mneut), 'eV.', str(nreal), '.nzbin', str(nzbin), '.', str_space, 'space.dat']) 
 
-    # read in data file  
-    if not silent: print('--- reading %s ---' % fhalo) 
-    x, y, z = np.loadtxt(fhalo, skiprows=1, unpack=True, usecols=[0,1,2]) 
-    xyz = np.zeros((3, len(x)))
-    
-    xyz[0,:] = x
-    xyz[1,:] = y 
-    xyz[2,:] = z
-    
-    delta = pySpec.FFTperiodic(xyz, fft='fortran', Lbox=Lbox, Ngrid=Ngrid, silent=silent) 
-    delta_fft = pySpec.reflect_delta(delta, Ngrid=Ngrid) 
-    
-    # calculate bispectrum 
-    i_k, j_k, l_k, b123, q123 = pySpec.Bk123_periodic(
-            delta_fft, Nmax=Nmax, Ncut=Ncut, step=step, fft_method='pyfftw', nthreads=1, silent=silent) 
-
-    hdr = ('halo bispectrum for mneut=%f, realization %i, redshift bin %i; k_f = 2pi/%f' % 
-            (mneut, nreal, nzbin, Lbox))
     fbk = ''.join([UT.dat_dir(), 'bispectrum/', 
          fhalo.split('/')[-1].rsplit('.dat', 1)[0],
         '.Ngrid', str(Ngrid), 
@@ -45,8 +28,30 @@ def haloBispectrum(mneut, nreal, nzbin, Lbox=1000., zspace=False,
         '.step', str(step), 
         '.pyfftw', 
         '.dat']) 
-    np.savetxt(fbk, np.array([i_k, j_k, l_k, b123, q123]).T, fmt='%i %i %i %.5e %.5e', 
-            delimiter='\t', header=hdr)
+    if not os.path.isfile(fbk) or overwrite: 
+        # read in data file  
+        if not silent: print('--- calculating %s ---' % fbk) 
+        if not silent: print('--- reading %s ---' % fhalo) 
+        x, y, z = np.loadtxt(fhalo, skiprows=1, unpack=True, usecols=[0,1,2]) 
+        xyz = np.zeros((3, len(x)))
+        
+        xyz[0,:] = x
+        xyz[1,:] = y 
+        xyz[2,:] = z
+        
+        delta = pySpec.FFTperiodic(xyz, fft='fortran', Lbox=Lbox, Ngrid=Ngrid, silent=silent) 
+        delta_fft = pySpec.reflect_delta(delta, Ngrid=Ngrid) 
+        
+        # calculate bispectrum 
+        i_k, j_k, l_k, b123, q123, cnts = pySpec.Bk123_periodic(
+                delta_fft, Nmax=Nmax, Ncut=Ncut, step=step, fft_method='pyfftw', nthreads=1, silent=silent) 
+
+        hdr = ('halo bispectrum for mneut=%f, realization %i, redshift bin %i; k_f = 2pi/%f' % 
+                (mneut, nreal, nzbin, Lbox))
+        np.savetxt(fbk, np.array([i_k, j_k, l_k, b123, q123, cnts]).T, fmt='%i %i %i %.5e %.5e %.5e', 
+                delimiter='\t', header=hdr)
+    else: 
+        if not silent: print('--- %s already exists ---' % fbk) 
     return None 
 
 
@@ -55,4 +60,4 @@ if __name__=="__main__":
     nreal = int(sys.argv[2]) 
     nzbin = int(sys.argv[3]) 
     haloBispectrum(mneut, nreal, nzbin, Lbox=1000., zspace=False, 
-            Ngrid=360, Nmax=40, Ncut=3, step=3, silent=True)
+            Ngrid=360, Nmax=40, Ncut=3, step=3, silent=True, overwrite=False)
