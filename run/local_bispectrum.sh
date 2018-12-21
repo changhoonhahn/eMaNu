d7f50389cb4a65971d04e4b79477aa2a58802c49
changhoonhahn <changhoonhahn@lbl.gov>
2018-12-21 11:48:46 -0500

    bispectrum instead of reduce bispectrum show significant signs of neutrinos

--- run/local_bispectrum.sh
+++ run/local_bispectrum.sh
@@ -2,12 +2,12 @@
 now=$(date +"%T") 
 echo "start time ... $now"
 
-for ireal in {21..25}; do 
-    for mneut in 0.0 0.06 0.1 0.15; do 
-        echo "$mneut ... $ireal"
-        python /Users/ChangHoon/projects/eMaNu/run/bispectrum.py mneut $mneut $ireal 4
-    done 
-    for sig8 in 0.798 0.807 0.818; do 
+for ireal in {1..25}; do 
+    #for mneut in 0.0 0.06 0.1 0.15; do 
+    #    echo "$mneut ... $ireal"
+    #    python /Users/ChangHoon/projects/eMaNu/run/bispectrum.py mneut $mneut $ireal 4
+    #done 
+    for sig8 in 0.822; do #0.798 0.807 0.818; do 
         echo "$sig8 ... $ireal"
         python /Users/ChangHoon/projects/eMaNu/run/bispectrum.py sigma8 $sig8 $ireal 4
     done 
