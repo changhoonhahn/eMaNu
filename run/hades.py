b8f0543d401b5e7003d116adbf662dce7aff4c97
changhoonhahn <changhoonhahn@lbl.gov>
2019-02-14 18:10:04 -0800

    rewriting some of the scripts with more care

---
+++ run/hades.py
@@ -0,0 +1,144 @@
+'''
+
+Scripts for dealing with the hades simulation bispectrum
+measurements. THis includes measuring the covariance matrix
+and fisher matrix. 
+
+
+'''
+import os 
+import glob
+import h5py 
+import numpy as np 
+# -- emanu --
+from emanu import util as UT
+
+
+def hades_Mnu_hdf5(mneut, nzbin=4, rsd=True): 
+    ''' write out HADES halo bispectrum for given mnu and redshift bin to 
+    hdf5 file for fast and easier access. 
+
+    :param mneut: 
+        Mnu value options are 0.0, 0.06, 0.1, 0.15 eV
+
+    :param nzbin: (default: 4) 
+        redshift bin number. So far only 4 is computed 
+    '''
+    # RSD or not  
+    if rsd: str_rsd = '.zspace'
+    else: str_rsd = '.rspace'
+    # hades B(k) directory 
+    hades_dir = os.path.join(UT.dat_dir(), 'bispectrum', 'hades') 
+    fbks = glob.glob(os.path.join(hades_dir, 
+        'groups.%seV.[0-9]*.nzbin%i.mhmin3200.0%s.Ngrid360.Nmax40.Ncut3.step3.pyfftw.dat' %
+        (str(mneut), nzbin, str_rsd)))
+    nbk = len(fbks) 
+
+    print('\n%i bispectrum files with Mnu=%.2feV zbin=%i in %s\n' % (nbk, mneut, nzbin, str_rsd.strip('.')))  
+    
+    assert nbk == 100, "not the correct number of files!"
+    
+    # load in all the files 
+    for i, fbk in enumerate(fbks):
+        i_k, j_k, l_k, _p0k1, _p0k2, _p0k3, b123, q123, b_sn, cnts = np.loadtxt(
+                os.path.join(hades_dir, fbk), skiprows=1, unpack=True, usecols=range(10)) 
+        if i == 0: 
+            p0k1 = np.zeros((nbk, len(i_k)))
+            p0k2 = np.zeros((nbk, len(i_k)))
+            p0k3 = np.zeros((nbk, len(i_k)))
+            bks = np.zeros((nbk, len(i_k)))
+            qks = np.zeros((nbk, len(i_k)))
+            bsn = np.zeros((nbk, len(i_k)))
+        p0k1[i,:] = _p0k1
+        p0k2[i,:] = _p0k2
+        p0k3[i,:] = _p0k3
+        bks[i,:] = b123
+        qks[i,:] = q123
+        bsn[i,:] = b_sn 
+
+    # save to hdf5 file 
+    fhdf5 = os.path.join(UT.dat_dir(), 'bispectrum', 'hades.%seV.nzbin%i.mhmin3200.0%s.hdf5' % (str(mneut), nzbin, str_rsd))
+    print('will be written to %s\n' % fhdf5)
+    f = h5py.File(fhdf5, 'w') 
+    f.create_dataset('k1', data=i_k)
+    f.create_dataset('k2', data=j_k)
+    f.create_dataset('k3', data=l_k)
+    f.create_dataset('p0k1', data=p0k1) 
+    f.create_dataset('p0k2', data=p0k2) 
+    f.create_dataset('p0k3', data=p0k3) 
+    f.create_dataset('b123', data=bks) 
+    f.create_dataset('q123', data=qks) 
+    f.create_dataset('b_sn', data=bsn) 
+    f.create_dataset('counts', data=cnts) 
+    f.close()
+    return None 
+
+
+def hades_sig8_hdf5(sig8, nzbin=4, rsd=True): 
+    ''' write out HADES halo bispectrum for given mnu and redshift bin to 
+    hdf5 file for fast and easier access. 
+
+    :param mneut: 
+        Mnu value options are 0.0, 0.06, 0.1, 0.15 eV
+
+    :param nzbin: (default: 4) 
+        redshift bin number. So far only 4 is computed 
+    '''
+    # RSD or not  
+    if rsd: str_rsd = '.zspace'
+    else: str_rsd = '.rspace'
+    # hades B(k) directory 
+    hades_dir = os.path.join(UT.dat_dir(), 'bispectrum', 'hades') 
+    fbks = glob.glob(os.path.join(hades_dir, 
+        'groups.0.0eV.sig8_%s.*.nzbin%i.mhmin3200.0%s.Ngrid360.Nmax40.Ncut3.step3.pyfftw.dat' %
+        (str(sig8), nzbin, str_rsd)))
+    nbk = len(fbks) 
+
+    print('\n%i bispectrum files with Mnu=0.0eV sigma8=%.2f zbin=%i in %s\n' % (nbk, sig8, nzbin, str_rsd.strip('.')))  
+    
+    assert nbk == 100, "not the correct number of files!"
+    
+    # load in all the files 
+    for i, fbk in enumerate(fbks):
+        i_k, j_k, l_k, _p0k1, _p0k2, _p0k3, b123, q123, b_sn, cnts = np.loadtxt(
+                os.path.join(hades_dir, fbk), skiprows=1, unpack=True, usecols=range(10)) 
+        if i == 0: 
+            p0k1 = np.zeros((nbk, len(i_k)))
+            p0k2 = np.zeros((nbk, len(i_k)))
+            p0k3 = np.zeros((nbk, len(i_k)))
+            bks = np.zeros((nbk, len(i_k)))
+            qks = np.zeros((nbk, len(i_k)))
+            bsn = np.zeros((nbk, len(i_k)))
+        p0k1[i,:] = _p0k1
+        p0k2[i,:] = _p0k2
+        p0k3[i,:] = _p0k3
+        bks[i,:] = b123
+        qks[i,:] = q123
+        bsn[i,:] = b_sn 
+
+    # save to hdf5 file 
+    fhdf5 = os.path.join(UT.dat_dir(), 'bispectrum', 
+            'hades.0.0eV.sig8_%s.nzbin%i.mhmin3200.0%s.hdf5' % (str(sig8), nzbin, str_rsd))
+    print('will be written to %s\n' % fhdf5)
+    f = h5py.File(fhdf5, 'w') 
+    f.create_dataset('k1', data=i_k)
+    f.create_dataset('k2', data=j_k)
+    f.create_dataset('k3', data=l_k)
+    f.create_dataset('p0k1', data=p0k1) 
+    f.create_dataset('p0k2', data=p0k2) 
+    f.create_dataset('p0k3', data=p0k3) 
+    f.create_dataset('b123', data=bks) 
+    f.create_dataset('q123', data=qks) 
+    f.create_dataset('b_sn', data=bsn) 
+    f.create_dataset('counts', data=cnts) 
+    f.close()
+    return None 
+
+
+if __name__=="__main__":
+    #for mnu in [0.0, 0.06, 0.1, 0.15]: 
+    #    hades_Mnu_hdf5(mnu, nzbin=4, rsd=False)
+    #    hades_Mnu_hdf5(mnu, nzbin=4, rsd=True)
+    for sig8 in [0.822, 0.818, 0.807, 0.798]:
+        hades_sig8_hdf5(sig8, nzbin=4, rsd=False)
+        hades_sig8_hdf5(sig8, nzbin=4, rsd=True)
--- run/quijote.py
+++ run/quijote.py
@@ -1,7 +1,8 @@
 '''
 
-Some scripts for dealing with the quijote simulation bispectrum
-measurements. 
+Scripts for dealing with the quijote simulation bispectrum
+measurements. THis includes measuring the covariance matrix
+and fisher matrix. 
 
 
 '''
@@ -32,19 +33,27 @@ mpl.rcParams['ytick.major.width'] = 1.5
 mpl.rcParams['legend.frameon'] = False
 
 
-def quijote_covariance(krange=[0.01, 0.5]):
-    ''' calculate the covariance matrix using the 15,000 fiducial 
-    bispetra.
+def quijote_covariance(krange=[0.01, 0.5], shotnoise=True):
+    ''' calculate the covariance matrix using the 15,000 fiducial bispetra.
+
+    :param krange: (default: [0.01, 0.5]) 
+        k range of k1, k2, k3:
+        
+    :param shotnoise: (default: True)
+        By default, shotnoise should be included in the covariance matrix. 
     '''
     kmin, kmax = krange  
-    fcov = os.path.join(UT.dat_dir(), 'bispectrum', 'quijote_Cov_bk.%.2f_%.2f.gmorder.hdf5' % (kmin, kmax))
+    if shotnoise: 
+        fcov = os.path.join(UT.dat_dir(), 'bispectrum', 'quijote_Cov_bk.sn.%.2f_%.2f.gmorder.hdf5' % (kmin, kmax))
+    else: 
+        fcov = os.path.join(UT.dat_dir(), 'bispectrum', 'quijote_Cov_bk.%.2f_%.2f.gmorder.hdf5' % (kmin, kmax))
 
     if os.path.isfile(fcov): 
         f = h5py.File(fcov, 'r') 
         C_bk = f['C_bk'].value # fiducial covariance matrix 
     else: 
         # read in fiducial bispectra
-        bk = quijote_bk('fiducial', krange=krange)
+        bk = quijote_bk('fiducial', krange=krange, shotnoise=shotnoise)
         C_bk = np.cov(bk.T) 
 
         # write C_bk to hdf5 file 
@@ -52,6 +61,30 @@ def quijote_covariance(krange=[0.01, 0.5]):
         f.create_dataset('C_bk', data=C_bk) 
         f.close()
     return C_bk  
+
+
+def quijote_Cov_SNcomparison(krange=[0.01, 0.5]): 
+    ''' Compare covariance with and without shotnoise. With shot noise 
+    is the correct covariance matrix.
+    '''
+    C_sn = quijote_covariance(krange=krange, shotnoise=True)
+    C_nosn = quijote_covariance(krange=krange, shotnoise=False)
+
+    # plot the covariance matrices 
+    fig = plt.figure(figsize=(10,5))
+    sub = fig.add_subplot(121)
+    cm = sub.pcolormesh(C_sn, norm=LogNorm(vmin=1e11, vmax=1e18))
+    sub.set_title(r'with shotnoise (correct)', fontsize=20)
+    
+    sub = fig.add_subplot(122)
+    cm = sub.pcolormesh(C_nosn, norm=LogNorm(vmin=1e11, vmax=1e18))
+    sub.set_title(r'without shotnoise (incorrect)', fontsize=20)
+    sub.set_yticklabels([])
+    fig.subplots_adjust(wspace=0.1, right=0.925)
+    cbar_ax = fig.add_axes([0.95, 0.15, 0.02, 0.7])
+    fig.colorbar(cm, cax=cbar_ax)
+    fig.savefig(os.path.join(UT.fig_dir(), 'quijote_Cov_SN.png'), bbox_inches='tight') 
+    return None 
 
 
 def qujjote_covariance_convergence(krange=[0.01, 0.5]): 
@@ -91,15 +124,32 @@ def qujjote_covariance_convergence(krange=[0.01, 0.5]):
     return None
 
 
-def quijote_bk(theta, krange=[0.01, 0.5]): 
+def quijote_bk(theta, krange=[0.01, 0.5], shotnoise=False): 
     ''' read in bispectra for specified theta 
+    
+    :param theta: 
+        string that specifies which quijote run. `theta==fiducial`  
+        for the fiducial set of parameters. 
+
+    :param krange: (default: [0.01, 0.5]) 
+        k range of k1, k2, and k3. 
+
+    :param shotnoise: (default: False)
+        If False, function returns shot noise *corrected* B(k) -- B(k)
+        If True, function returns shot noise *uncorrected* B(k) -- \hat{B(k)}
+        where \hat{B(k)} = B(k) + B_SN 
     '''
     kmin, kmax = krange 
     # read in fiducial biectra
     dir_bk = os.path.join(UT.dat_dir(), 'bispectrum') 
     bks = h5py.File(os.path.join(dir_bk, 'quijote_%s.hdf5' % theta), 'r') 
     i_k, j_k, l_k = bks['k1'].value, bks['k2'].value, bks['k3'].value 
-    bk = bks['b123'].value 
+    bk_sn = bks['b_sn'].value  # shot noise 
+
+    if shotnoise: 
+        bk = bks['b123'].value + bk_sn # shot noise uncorrected
+    else: 
+        bk = bks['b123'].value # shot noise corrected
 
     # k range limit of the bispectrum
     kf = 2.*np.pi/1000. # fundmaentla mode
@@ -115,22 +165,44 @@ def quijote_bk(theta, krange=[0.01, 0.5]):
 ##################################################################
 # qujiote fisher stuff
 ##################################################################
-def quijote_Fisher(krange=[0.01, 0.5], deriv='p', validate=False): 
+def quijote_Fisher(krange=[0.01, 0.5], deriv='p', shotnoise=True, validate=False): 
     ''' calculate fisher matrix for parameters ['Om', 'Ob', 'h', 'ns', 's8', 'Mnu']
     '''
     kmin, kmax = krange
     thetas = ['Om', 'Ob', 'h', 'ns', 's8', 'Mnu']
     theta_lbls = [r'$\Omega_m$', r'$\Omega_b$', r'$h$', r'$n_s$', r'$\sigma_8$', r'$M_\nu$']
+
+    C_fid = quijote_covariance(krange=krange, shotnoise=shotnoise) # fiducial covariance (with shotnoise is the correct one) 
+    C_inv = np.linalg.inv(C_fid) # invert the covariance 
+
+    dbk_dt = [] 
+    for par in thetas:  # read in in dBk/dtheta_i 
+        str_d = '' 
+        if par == 'Mnu': str_d = '.'+deriv
+        f = h5py.File(os.path.join(UT.dat_dir(), 'bispectrum', 
+            'quijote_dbk_d%s%s.%.2f_%.2f.gmorder.hdf5' % (par, str_d, kmin, kmax)), 'r') 
+        dbk_dt.append(f['dbk_dtheta'].value)
+    
     Fij = np.zeros((len(thetas), len(thetas)))
     for i, par_i in enumerate(thetas): 
         for j, par_j in enumerate(thetas): 
-            fij = quijote_Fij(par_i, par_j, deriv=deriv, krange=krange)
+            dbk_dtt_i = dbk_dt[i]
+            dbk_dtt_j = dbk_dt[j]
+
+            # calculate Mij 
+            Mij = np.dot(dbk_dtt_i[:,None], dbk_dtt_j[None,:]) + np.dot(dbk_dtt_j[:,None], dbk_dtt_i[None,:])
+
+            fij = 0.5 * np.trace(np.dot(C_inv, Mij))
             Fij[i,j] = fij 
     
-    bk_dir = os.path.join(UT.dat_dir(), 'bispectrum')
-    f_ij = os.path.join(bk_dir, 'quijote_Fisher.%s.%.2f_%.2f.gmorder.hdf5' % (deriv, kmin, kmax))
+    sn_str = ''
+    if shotnoise: sn_str = '.sn'
+    f_ij = os.path.join(UT.dat_dir(), 'bispectrum', 
+            'quijote_Fisher%s.%s.%.2f_%.2f.gmorder.hdf5' % (sn_str, deriv, kmin, kmax))
     f = h5py.File(f_ij, 'w') 
     f.create_dataset('Fij', data=Fij) 
+    f.create_dataset('C_fid', data=C_fid)
+    f.create_dataset('C_inv', data=C_inv)
     f.close() 
 
     if validate: 
@@ -147,73 +219,6 @@ def quijote_Fisher(krange=[0.01, 0.5], deriv='p', validate=False):
         fig.colorbar(cm)
         fig.savefig(f_ij.replace('.hdf5', '.png'), bbox_inches='tight') 
     return None
-
-
-def quijote_Fij(theta_i, theta_j, deriv='p', krange=[0.01, 0.5]): 
-    ''' calculate the i, j term of the fisher matrix. Fij is typically 
-    calculated as:  
-    
-    Fij = 1/2 * Tr[C^-1 C,i C^-1 C,j + C^-1 Mij]    where
-    Mij = dBk/dtheta_i dBk/dtheta_j.T + dBk/dtheta_j dBk/dtheta_i.T
-
-    However, for our purposes we will ignore the first term and compute
-
-    Fij = 1/2 * Tr[C^{-1} Mij] 
-
-    :param theta_i: 
-        string specificying theta_i 
-
-    :param theta_j: 
-        string specificying theta_j 
-
-    :param deriv: (default: 'p') 
-        string specifying how to compute the Mnu derivative. The options are 
-        'p', 'pp', 'ppp', and 'fd'
-    
-    :param krange: (default: [0.01, 0.5]) 
-        k-range of the bispectrum measurements. Note that the
-        Gil-Marin triangle order will also be implemented. 
-    '''
-    kmin, kmax = krange
-    bk_dir = os.path.join(UT.dat_dir(), 'bispectrum')
-    if 'Mnu' in [theta_i, theta_j]: 
-        fij = os.path.join(bk_dir, 'Fij', 'quijote_Fij.%s.%s.%s.%.2f_%.2f.gmorder.hdf5' % 
-                (theta_i, theta_j, deriv, kmin, kmax))
-    else: 
-        fij = os.path.join(bk_dir, 'Fij', 'quijote_Fij.%s.%s.%.2f_%.2f.gmorder.hdf5' % 
-                (theta_i, theta_j, kmin, kmax))
-
-    if os.path.isfile(fij): 
-        f = h5py.File(fij, 'r') 
-        Fij = f['Fij'].value 
-    else: 
-        str_di, str_dj = '', ''
-        if theta_i == 'Mnu': str_di = '.'+deriv
-        if theta_j == 'Mnu': str_dj = '.'+deriv
-
-        C_fid = quijote_covariance(krange=krange) # fiducial covariance
-        Cinv = np.linalg.inv(C_fid) # invert the covariance 
-        
-        fdbk_i = os.path.join(bk_dir, 'quijote_dbk_d%s%s.%.2f_%.2f.gmorder.hdf5' % (theta_i, str_di, kmin, kmax))
-        fdbk_j = os.path.join(bk_dir, 'quijote_dbk_d%s%s.%.2f_%.2f.gmorder.hdf5' % (theta_j, str_dj, kmin, kmax))
-
-        # read in dBk/dtheta_i and dBk/dtheta_j 
-        f_i = h5py.File(fdbk_i, 'r') 
-        dbk_dtt_i = f_i['dbk_dtheta'].value
-        f_j = h5py.File(fdbk_j, 'r') 
-        dbk_dtt_j = f_j['dbk_dtheta'].value
-
-        # calculate Mij 
-        Mij = np.dot(dbk_dtt_i[:,None], dbk_dtt_j[None,:]) + np.dot(dbk_dtt_j[:,None], dbk_dtt_i[None,:])
-
-        Fij = 0.5 * np.trace(np.dot(Cinv, Mij))
-
-        f = h5py.File(fij, 'w') 
-        f.create_dataset('Fij', data=Fij) 
-        f.create_dataset('Cinv', data=Cinv) 
-        f.create_dataset('Mij', data=Mij) 
-        f.close() 
-    return Fij 
 
 
 def quijote_dBk(theta, krange=[0.01, 0.5], validate=False):
@@ -490,10 +495,12 @@ if __name__=="__main__":
     #    quijote_comparison(par, krange=[0.01, 0.5])
     
     # covariance matrix 
-    #quijote_covariance(krange=[0.01, 0.5])
-    qujjote_covariance_convergence(krange=[0.01, 0.5])
-    #for kmax in [0.2, 0.3, 0.4, 0.5]: 
+    #quijote_covariance(krange=[0.01, 0.5], shotnoise=True) # covariance matrix with shotnoise (i.e. correct covariance) 
+    #quijote_Cov_SNcomparison(krange=[0.01, 0.5])
+    #qujjote_covariance_convergence(krange=[0.01, 0.5])
+    for kmax in [0.2, 0.3, 0.4, 0.5]: 
+        for deriv in ['p', 'fd']: 
+            quijote_Fisher(krange=[0.01, kmax], deriv=deriv, shotnoise=True, validate=True)   
+            quijote_Fisher(krange=[0.01, kmax], deriv=deriv, shotnoise=False, validate=True)   
     #    for par in ['Mnu', 'Ob', 'Om', 'h', 'ns', 's8']: 
     #        quijote_dBk(par, krange=[0.01, kmax], validate=True)
-    #    for deriv in ['p', 'pp', 'ppp', 'fd']: 
-    #        quijote_Fisher(krange=[0.01, kmax], deriv=deriv, validate=True)   
