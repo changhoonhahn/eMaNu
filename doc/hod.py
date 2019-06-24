25298a914325af39031fdd4d719fd370725bc33a
changhoonhahn <changhoonhahn@lbl.gov>
2019-06-24 16:26:08 -0700

    plots to decide on fiducial HOD parameters

--- doc/hod.py
+++ doc/hod.py
@@ -164,12 +164,13 @@ def _plot_wp_model(theta, Mr=-21.5, low_sigma=False):
         tt = np.array([14.22, 0.77, 14.00, 0.87, 14.69])
     else: 
         raise NotImplementedError
-    if low_sigma: tt[1] = 0.2 
+    if low_sigma: tt[1] = low_sigma 
     
     tt_dict = {'logMmin': tt[0], 'sigma_logM': tt[1], 'logM0': tt[2], 'alpha': tt[3], 'logM1': tt[4]}
     hod = FM.hodGalaxies(halos, tt_dict)
     fmlim = float(np.sum(np.array(hod['halo_mvir']) < 10**Mh_min))/float(len(np.array(hod['halo_mvir'])))
-    print fmlim 
+    Nmlim = np.sum(np.array(hod['halo_mvir']) < 10**Mh_min)
+    print fmlim, Nmlim 
 
     fig = plt.figure(figsize=(10,5))
     sub = fig.add_subplot(121)
@@ -181,6 +182,8 @@ def _plot_wp_model(theta, Mr=-21.5, low_sigma=False):
     sub.plot(_Mhbins, Ncen+Nsat, c='k', ls=':', lw=1) 
     sub.plot([10**Mh_min, 10**Mh_min], [1e-2, 1e1], c='k', ls='--')
     sub.text(0.2, 0.95, r'%.1e of halos below $M_{\rm min}$' % fmlim, 
+            ha='left', va='top', transform=sub.transAxes, fontsize=15)
+    sub.text(0.2, 0.85, r'%i halos' % Nmlim, 
             ha='left', va='top', transform=sub.transAxes, fontsize=15)
     sub.set_xlabel('$M_h$', fontsize=25) 
     sub.set_xscale('log') 
@@ -201,10 +204,246 @@ def _plot_wp_model(theta, Mr=-21.5, low_sigma=False):
     sub.set_xlim(1e-1, 25) 
     sub.set_ylabel(r'$w_p(r_p)$', fontsize=25) 
     sub.set_yscale('log') 
-    #sub.set_ylim(2, 1e3) 
+    sub.set_ylim(2, 5e3) 
     fig.subplots_adjust(wspace=0.3) 
     if not low_sigma: ffig = os.path.join(dir_fig, 'wp_model.%s.png' % theta)
-    else: ffig = os.path.join(dir_fig, 'wp_model.%s.lowsigma.png' % theta)
+    else: ffig = os.path.join(dir_fig, 'wp_model.%s.sig%.1f.png' % (theta, low_sigma))
+    fig.savefig(ffig, bbox_inches='tight') 
+    plt.close() 
+    return None 
+
+
+def _plot_wp_model_pm(theta, Mr=-21.5, low_sigma=False): 
+    ''' plot wp of models for some arbitrary parameters and compare with SDSS wp
+    '''
+    # sdss wp 
+    rp, _wp_sdss, cov = wp_sdss(Mr=Mr) 
+    # r bin  
+    rbins = np.loadtxt(os.path.join(dir_hod, 'rbins.dat'), unpack=True, usecols=[0]) 
+    # halo catalogs 
+    halos = hadesData.hadesMnuHalos(0., 1, 4, 
+            mh_min=None, dir='/Users/ChangHoon/data/emanu/halos/hades/0.0eV/1')
+    Mh_min = np.log10(np.array(halos['Mass']).min()) 
+    # hi res halo catalog
+    halos_hires = hadesData.hadesMnuHalos(0., '1_hires', 4, 
+            mh_min=None, dir='/Users/ChangHoon/data/emanu/halos/hades/0.0eV/1_hires')
+
+    dtt = np.array([0.1, 0.1, 0.3, 0.3, 0.1]) 
+    if theta == 'zheng2007': 
+        lbl = 'Zheng+(2007)'
+        tt  = np.array([13.38, 0.51, 13.94, 1.04, 13.91])
+    elif theta == 'guo2015': 
+        lbl = 'Guo+(2015)'
+        tt  = np.array([13.53, 0.72, 13.13, 1.14, 14.52])
+    elif theta == 'vakili2019': 
+        lbl = 'Vakili+(2019)'
+        tt  = np.array([13.39, 0.56, 12.87, 1.26, 14.51])
+    elif theta == 'zhengMr22':
+        lbl = 'Zheng+(2007) $M_r = -22$'
+        tt  = np.array([14.22, 0.77, 14.00, 0.87, 14.69])
+    else: 
+        raise NotImplementedError
+    ttm = tt - dtt
+    ttp = tt + dtt 
+    if low_sigma: 
+        tt[1]  = low_sigma 
+        ttm[1] = low_sigma - dtt[1]
+        ttp[1] = low_sigma + dtt[1]
+    lbls = [r'$\log M_{\rm min}$', r'$\sigma_{\log M}$',r'$\log M_0$',r'$\alpha$',r'$\log M_1$']
+    names = ['logMmin', 'sig_logM', 'logM0', 'alpha', 'logM1']
+    
+    tt_fid = tt.copy()
+    tt_dict = {'logMmin': tt_fid[0], 'sigma_logM': tt_fid[1], 'logM0': tt_fid[2], 'alpha': tt_fid[3], 'logM1': tt_fid[4]}
+    hod = FM.hodGalaxies(halos, tt_dict)
+    fmlim = float(np.sum(np.array(hod['halo_mvir']) < 10**Mh_min))/float(len(np.array(hod['halo_mvir'])))
+    Nmlim = np.sum(np.array(hod['halo_mvir']) < 10**Mh_min)
+    print tt_fid
+    print('fiducial, %.3f, %i' % (fmlim, Nmlim))
+
+    _Mhbins = np.logspace(11, 15, 300)
+    Ncen_fid = Ncen_Mh(tt_fid, np.log10(_Mhbins)) 
+    Nsat_fid = Nsat_Mh(tt_fid, np.log10(_Mhbins)) 
+    wp_fid = wp_model(halos, tt_fid, rsd=True) 
+    wp_fid_hires = wp_model(halos_hires, tt_fid, rsd=True) 
+    
+    fig = plt.figure(figsize=(12,25))
+    for i in range(5): 
+        _ttm = tt.copy() 
+        _ttm[i] = ttm[i] 
+        sub = fig.add_subplot(5,2,2*i+1)
+        tt_dict = {'logMmin': _ttm[0], 'sigma_logM': _ttm[1], 
+                'logM0': _ttm[2], 'alpha': _ttm[3], 'logM1': _ttm[4]}
+        hod = FM.hodGalaxies(halos, tt_dict)
+        fmlim = float(np.sum(np.array(hod['halo_mvir']) < 10**Mh_min))/float(len(np.array(hod['halo_mvir'])))
+        Nmlim = np.sum(np.array(hod['halo_mvir']) < 10**Mh_min)
+        print('%s- %.3f, %i' % (names[i], fmlim, Nmlim))
+        _ttp = tt.copy() 
+        _ttp[i] = ttp[i] 
+        tt_dict = {'logMmin': _ttp[0], 'sigma_logM': _ttp[1], 
+                'logM0': _ttp[2], 'alpha': _ttp[3], 'logM1': _ttp[4]}
+        hod = FM.hodGalaxies(halos, tt_dict)
+        fmlim = float(np.sum(np.array(hod['halo_mvir']) < 10**Mh_min))/float(len(np.array(hod['halo_mvir'])))
+        Nmlim = np.sum(np.array(hod['halo_mvir']) < 10**Mh_min)
+        print('%s+ %.3f, %i' % (names[i], fmlim, Nmlim))
+
+        sub.plot(_Mhbins, Ncen_fid, c='k', ls='--', lw=1) 
+        sub.plot(_Mhbins, Nsat_fid, c='k', ls=':', lw=1) 
+        Ncen = Ncen_Mh(_ttm, np.log10(_Mhbins)) 
+        Nsat = Nsat_Mh(_ttm, np.log10(_Mhbins)) 
+        sub.plot(_Mhbins, Ncen, c='C1', lw=1, ls='--') 
+        sub.plot(_Mhbins, Nsat, c='C1', lw=1, ls=':') 
+        Ncen = Ncen_Mh(_ttp, np.log10(_Mhbins)) 
+        Nsat = Nsat_Mh(_ttp, np.log10(_Mhbins)) 
+        sub.plot(_Mhbins, Ncen, c='C0', lw=1, ls='--') 
+        sub.plot(_Mhbins, Nsat, c='C0', lw=1, ls=':') 
+        sub.plot([10**Mh_min, 10**Mh_min], [1e-2, 1e1], c='k', ls='--')
+        #sub.text(0.2, 0.95, r'%.1e of halos below $M_{\rm min}$' % fmlim, 
+        #        ha='left', va='top', transform=sub.transAxes, fontsize=15)
+        #sub.text(0.2, 0.85, r'%i halos' % Nmlim, 
+        #        ha='left', va='top', transform=sub.transAxes, fontsize=15)
+        if i == 4: sub.set_xlabel('$M_h$', fontsize=25) 
+        sub.set_xscale('log') 
+        sub.set_xlim(5e12, 1e15) 
+        sub.set_ylabel(r'$<N_{\rm gal}>$', fontsize=25) 
+        sub.set_yscale('log') 
+        sub.set_ylim(1e-1, 1e1) 
+
+        sub = fig.add_subplot(5,2,2*i+2)
+        sub.plot(rp, wp_fid, c='k') 
+        sub.plot(rp, wp_fid_hires, c='k', ls=':') 
+        _wp = wp_model(halos, _ttm, rsd=True) 
+        sub.plot(rp, _wp, c='C1', label=r'$\theta_-$') 
+        _wp = wp_model(halos, _ttp, rsd=True) 
+        sub.plot(rp, _wp, c='C0', label=r'$\theta_+$') 
+        _wp = wp_model(halos_hires, _ttm, rsd=True) 
+        sub.plot(rp, _wp, c='C1', ls=':') 
+        _wp = wp_model(halos_hires, _ttp, rsd=True) 
+        sub.plot(rp, _wp, c='C0', ls=':', label='High Res.') 
+        if i == 0: sub.legend(loc='lower left', fontsize=15) 
+        sub.errorbar(rp, _wp_sdss, yerr=np.sqrt(np.diag(cov)), fmt='.k') 
+        if i == 4: sub.set_xlabel(r'$r_p$ [$h^{-1}{\rm Mpc}$]', fontsize=25) 
+        sub.text(0.95, 0.95, lbls[i], ha='right', va='top', transform=sub.transAxes, fontsize=20)
+        sub.set_xscale('log') 
+        sub.set_xlim(1e-1, 25) 
+        sub.set_ylabel(r'$w_p(r_p)$', fontsize=25) 
+        sub.set_yscale('log') 
+        sub.set_ylim(2, 5e3) 
+    fig.subplots_adjust(wspace=0.3) 
+    if not low_sigma: ffig = os.path.join(dir_fig, 'wp_model_pm.%s.png' % theta)
+    else: ffig = os.path.join(dir_fig, 'wp_model_pm.%s.sig%.1f.png' % (theta, low_sigma))
+    fig.savefig(ffig, bbox_inches='tight') 
+    plt.close() 
+    return None 
+
+
+def _plot_wp_model_pm_fiducial(): 
+    ''' plot wp of models for fiducial parameter and the derivatives 
+    '''
+    rp, _wp_sdss, cov = wp_sdss(Mr=-21.5) 
+    # r bin  
+    rbins = np.loadtxt(os.path.join(dir_hod, 'rbins.dat'), unpack=True, usecols=[0]) 
+    # halo catalogs 
+    halos = hadesData.hadesMnuHalos(0., 1, 4, 
+            mh_min=None, dir='/Users/ChangHoon/data/emanu/halos/hades/0.0eV/1')
+    Mh_min = np.log10(np.array(halos['Mass']).min()) 
+    # hi res halo catalog
+    halos_hires = hadesData.hadesMnuHalos(0., '1_hires', 4, 
+            mh_min=None, dir='/Users/ChangHoon/data/emanu/halos/hades/0.0eV/1_hires')
+
+    dtt = np.array([0.1, 0.1, 0.3, 0.3, 0.1]) 
+    tt  = np.array([13.85, 0.175, 13.94, 0.95, 14.3]) 
+    ttm = tt - dtt
+    ttp = tt + dtt 
+
+    lbls = [r'$\log M_{\rm min}$', r'$\sigma_{\log M}$',r'$\log M_0$',r'$\alpha$',r'$\log M_1$']
+    names = ['logMmin', 'sig_logM', 'logM0', 'alpha', 'logM1']
+    
+    _Mhbins = np.logspace(11, 16, 200)
+    Ncen_fid = Ncen_Mh(tt, np.log10(_Mhbins)) 
+    Nsat_fid = Nsat_Mh(tt, np.log10(_Mhbins)) 
+    wp_fid = wp_model(halos, tt, rsd=True) 
+    wp_fid_hires = wp_model(halos_hires, tt, rsd=True) 
+
+    tt_z07_21p5 = np.array([13.38, 0.51, 13.94, 1.04, 13.91])
+    tt_z07_22p0 = np.array([14.22, 0.77, 14.00, 0.87, 14.69])
+    Ncen_z07_21p5 = Ncen_Mh(tt_z07_21p5, np.log10(_Mhbins))
+    Nsat_z07_21p5 = Nsat_Mh(tt_z07_21p5, np.log10(_Mhbins))
+    Ncen_z07_22p0 = Ncen_Mh(tt_z07_22p0, np.log10(_Mhbins))
+    Nsat_z07_22p0 = Nsat_Mh(tt_z07_22p0, np.log10(_Mhbins))
+    
+    fig = plt.figure(figsize=(40,8))
+    for i in range(5): 
+        _ttm = tt.copy() 
+        _ttm[i] = ttm[i] 
+        sub = fig.add_subplot(2,5,i+1)
+        tt_dict = {'logMmin': _ttm[0], 'sigma_logM': _ttm[1], 
+                'logM0': _ttm[2], 'alpha': _ttm[3], 'logM1': _ttm[4]}
+        hod = FM.hodGalaxies(halos_hires, tt_dict)
+        fmlim = float(np.sum(np.array(hod['halo_mvir']) < 10**Mh_min))/float(len(np.array(hod['halo_mvir'])))
+        Nmlim = np.sum(np.array(hod['halo_mvir']) < 10**Mh_min)
+        print('%s- %.4f, %i' % (names[i], fmlim, Nmlim))
+        _ttp = tt.copy() 
+        _ttp[i] = ttp[i] 
+        tt_dict = {'logMmin': _ttp[0], 'sigma_logM': _ttp[1], 
+                'logM0': _ttp[2], 'alpha': _ttp[3], 'logM1': _ttp[4]}
+        hod = FM.hodGalaxies(halos_hires, tt_dict)
+        fmlim = float(np.sum(np.array(hod['halo_mvir']) < 10**Mh_min))/float(len(np.array(hod['halo_mvir'])))
+        Nmlim = np.sum(np.array(hod['halo_mvir']) < 10**Mh_min)
+        print('%s+ %.4f, %i' % (names[i], fmlim, Nmlim))
+
+        sub.plot(_Mhbins, Ncen_fid + Nsat_fid, c='k', ls='-', lw=1) 
+        Ncen = Ncen_Mh(_ttm, np.log10(_Mhbins)) 
+        Nsat = Nsat_Mh(_ttm, np.log10(_Mhbins)) 
+        sub.plot(_Mhbins, Ncen + Nsat, c='C1', lw=1, ls='-') 
+        Ncen = Ncen_Mh(_ttp, np.log10(_Mhbins)) 
+        Nsat = Nsat_Mh(_ttp, np.log10(_Mhbins)) 
+        sub.plot(_Mhbins, Ncen + Nsat, c='C0', lw=1, ls='-') 
+
+        plt_21p5, = sub.plot(_Mhbins, Ncen_z07_21p5 + Nsat_z07_21p5, c='k', ls=':', lw=1) 
+        plt_22p0, = sub.plot(_Mhbins, Ncen_z07_22p0 + Nsat_z07_22p0, c='k', ls='--', lw=1) 
+
+        sub.fill_between([10**12, 10**Mh_min], [1e-2, 1e-2], [1e2, 1e2], color='k', linewidth=0, alpha=0.5)
+        #if i == 4: sub.set_xlabel('$M_h$', fontsize=25) 
+        sub.set_xscale('log') 
+        sub.set_xlim(5e12, 5e15) 
+        sub.set_yscale('log') 
+        sub.set_ylim(1e-1, 1e2) 
+        if i == 0: sub.set_ylabel(r'$<N_{\rm gal}>$', fontsize=25) 
+        else: sub.set_yticklabels([]) 
+        if i == 4: sub.legend([plt_21p5, plt_22p0], ['SDSS $M_r < -21.5$', 'SDSS $M_r < -22$'],
+                loc='upper left', fontsize=12) 
+
+        sub = fig.add_subplot(2,5,i+6)
+        sub.plot(rp, wp_fid, c='k') 
+        sub.plot(rp, wp_fid_hires, c='k', ls=':') 
+        _wp = wp_model(halos, _ttm, rsd=True) 
+        sub.plot(rp, _wp, c='C1', label=r'$\theta_-$') 
+        _wp = wp_model(halos, _ttp, rsd=True) 
+        sub.plot(rp, _wp, c='C0', label=r'$\theta_+$') 
+        _wp = wp_model(halos_hires, _ttm, rsd=True) 
+        sub.plot(rp, _wp, c='C1', ls=':') 
+        _wp = wp_model(halos_hires, _ttp, rsd=True) 
+        sub.plot(rp, _wp, c='C0', ls=':', label='High Res.') 
+        if i == 4: sub.legend(loc='lower left', fontsize=15) 
+        
+        sub.text(0.95, 0.95, '%s=$%s\pm %.1f$' % (lbls[i], str(tt[i]), dtt[i]), 
+                ha='right', va='top', transform=sub.transAxes, fontsize=15)
+        sub.set_xscale('log') 
+        sub.set_xlim(1e-1, 25) 
+        sub.set_yscale('log') 
+        sub.set_ylim(1e1, 3e4) 
+        if i == 0: sub.set_ylabel(r'$w_p(r_p)$', fontsize=25) 
+        else: sub.set_yticklabels([]) 
+
+    bkgd = fig.add_subplot(211, frameon=False)
+    bkgd.set_xlabel('$M_h$', fontsize=25) 
+    bkgd.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+    bkgd = fig.add_subplot(212, frameon=False)
+    bkgd.set_xlabel(r'$r_p$ [$h^{-1}{\rm Mpc}$]', fontsize=25) 
+    bkgd.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+
+    fig.subplots_adjust(wspace=0.05, hspace=0.3) 
+    ffig = os.path.join(dir_fig, 'wp_model_pm.fiducial.png')
     fig.savefig(ffig, bbox_inches='tight') 
     plt.close() 
     return None 
@@ -214,7 +453,9 @@ def Ncen_Mh(tt, logMh):
     ''' expected Ncen at Mh 
     '''
     logMmin, sig_logM, _, _, _ = tt
-    return 0.5 * (1. + sp.special.erf((logMh - logMmin)/sig_logM)) 
+    Ncen = 0.5 * (1. + sp.special.erf((logMh - logMmin)/sig_logM)) 
+    Ncen[~np.isfinite(Ncen)] = 0. 
+    return Ncen
 
 
 def Nsat_Mh(tt, logMh): 
@@ -222,7 +463,9 @@ def Nsat_Mh(tt, logMh):
     '''
     _Ncen = Ncen_Mh(tt, logMh) 
     _, _, logM0, alpha, logM1 = tt
-    return _Ncen * ((10**logMh - 10**logM0)/10**logM1)**alpha
+    Nsat = _Ncen * ((10**logMh - 10**logM0)/10**logM1)**alpha
+    Nsat[~np.isfinite(Nsat)] = 0. 
+    return Nsat 
 
 
 def wp_sdss(Mr=-21.5): 
@@ -278,7 +521,11 @@ def fvolume(Mr=-21.5):
 
 if __name__=='__main__': 
     #_plot_wp_sdss(Mr=-21.5)
-    for tt in ['zheng2007', 'guo2015', 'vakili2019', 'zhengMr22']: 
-        _plot_wp_model(tt, Mr=-21.5, low_sigma=False)
-        _plot_wp_model(tt, Mr=-21.5, low_sigma=True)
+    #for tt in ['guo2015']:#, 'vakili2019', 'zhengMr22']: #'zheng2007', 
+    #    print tt 
+    #    _plot_wp_model_pm(tt, Mr=-21.5, low_sigma=False)
+    #    _plot_wp_model_pm(tt, Mr=-21.5, low_sigma=0.2)
+    #    _plot_wp_model_pm(tt, Mr=-21.5, low_sigma=0.1)
+    _plot_wp_model_pm_fiducial()
+
     #hod_fit(Mr=-21.5)
