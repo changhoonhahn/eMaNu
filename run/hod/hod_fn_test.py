f0ec57888d39c7fce01b782974090e788dd92eb3
chang.nersc <changh20@gmail.com>
2019-07-19 12:17:56 -0700

    hod pipeline and bk caluclation confirmed on cori with new conda env

--- run/hod/hod_fn_test.py
+++ run/hod/hod_fn_test.py
@@ -4,8 +4,11 @@ script for testing HOD pipeline in new env
 conda activate hodtest
 # required packages
 - eMaNu
-- pySpectrum
 - nbodykit
+
+# bispectrum requires 
+- pySpectrum 
+- pyfftw
 
 '''
 import os 
@@ -13,11 +16,13 @@ import numpy as np
 # --- eMaNu --- 
 from emanu import forwardmodel as FM
 from emanu.sims import data as simData
+# -- pySpectrum -- 
+from pyspectrum import pyspectrum as pySpec
 
 
-def HOD(tt_hod, folder, snapnum, do_RSD, seed, Ob, ns, s8):
+def HOD(tt_hod, folder, snapnum, do_RSD, seed): #, Ob, ns, s8):
     # halo catalogs 
-    halos = simData.hqHalos(folder, snapnum, Ob=Ob, ns=ns, s8=s8)
+    halos = simData.hqHalos(folder, snapnum)#, Ob=Ob, ns=ns, s8=s8)
     # populate halos 
     hod = FM.hodGalaxies(halos, {'logMmin': tt_hod[0], 'sigma_logM': tt_hod[1], 'logM0': tt_hod[2], 'alpha': tt_hod[3], 'logM1': tt_hod[4]}, seed=seed) 
     # apply RSD 
@@ -27,9 +32,20 @@ def HOD(tt_hod, folder, snapnum, do_RSD, seed, Ob, ns, s8):
 
 
 if __name__=="__main__": 
-    tt_hod = np.array([14.30, 0.45, 14.10, 0.87, 14.80])
-    folder = '/Users/ChangHoon/data/emanu/halos/hades/0.0eV/1'
+    tt_hod = np.array([13.65, 0.2, 14., 1.1, 14.]) # fiducial HOD 
+    #folder = '/Users/ChangHoon/data/emanu/halos/hades/0.0eV/1'
+    folder  = '/global/cscratch1/sd/chahah/emanu/0.0eV/1' # on cori 
     snapnum = 4
-    do_RSD = True
-    seed = 0 
-    xyz = HOD(tt_hod, folder, snapnum, do_RSD, seed, 0.049, 0.9624, 0.834)
+    do_RSD  = True
+    seed    = 0 
+    xyz = HOD(tt_hod, folder, snapnum, do_RSD, seed)
+    print(xyz[:10]) 
+    raise ValueError
+    # calculate bispectrum
+    BoxSize = 1000. 
+    Ngrid   = 360
+    Nmax = 40
+    Ncut = 3
+    step = 3
+    b123out = pySpec.Bk_periodic(xyz.T, Lbox=BoxSize, Ngrid=Ngrid, step=step, 
+            Ncut=Ncut, Nmax=Nmax, fft='pyfftw', nthreads=8, silent=False) 
