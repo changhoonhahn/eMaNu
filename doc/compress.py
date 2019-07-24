35a59e2c207a77b26d158cb149f1bbf84469567d
changhoonhahn <changhoonhahn@lbl.gov>
2019-07-24 16:12:47 -0700

    compressor class object implemented with KL compression and PCA compression; still making sense of PCA compression results...

--- doc/compress.py
+++ doc/compress.py
@@ -14,6 +14,7 @@ from sklearn.decomposition import PCA
 from emanu import util as UT
 from emanu import obvs as Obvs
 from emanu import plots as ePlots
+from emanu import compressor as Comp
 from emanu import forecast as Forecast
 # --- plotting --- 
 import matplotlib as mpl
@@ -41,23 +42,22 @@ dir_doc = os.path.join(UT.doc_dir(), 'paper3', 'figs') # figures for paper
 dir_fig = os.path.join(UT.fig_dir(), 'compress')  # figure directory
 
 
-def Pk_compression(kmax=0.5):
+def Pk_compression(method='KL', kmax=0.5):
     ''' Comparison of compressed P0 versus full P0
     '''
     # read in Quijote P0
     quij    = Obvs.quijotePk('fiducial', rsd=0, flag='reg') 
     # k limit 
-    k = quij['k'] 
-    klim = (k <= kmax) 
+    klim = (quij['k'] <= kmax) 
     # calculate P0 covariance
-    pks     = quij['p0k'] + quij['p_sn'][:,None] # shotnoise uncorrected P(k)
+    pks     = quij['p0k'][:,klim] + quij['p_sn'][:,None] # shotnoise uncorrected P(k)
     Cpk     = np.cov(pks.T) 
-    Cpk     = Cpk[klim,:][:,klim]
+    Cinv    = np.linalg.inv(Cpk) 
     if np.linalg.cond(Cpk) >= 1e16: print('Covariance matrix is ill-conditioned') 
     ndata       = Cpk.shape[0]
     nmock       = pks.shape[0]
     f_hartlap   = float(nmock - ndata - 2)/float(nmock - 1) 
-    Cinv        = f_hartlap * np.linalg.inv(Cpk) 
+    Cinv        *= f_hartlap
     
     # derivative of P w.r.t theta
     dpkdt = [] 
@@ -75,28 +75,28 @@ def Pk_compression(kmax=0.5):
     Finvs = []
     Nmocks = [100, 250, 500, 1000, 5000][::-1]
     for Nmock in Nmocks: 
-        # KL compression matrix 
-        _Cpk     = np.cov(pks[:Nmock].T) 
-        _Cpk     = _Cpk[klim,:][:,klim]
-        if np.linalg.cond(_Cpk) >= 1e16: print('Covariance matrix is ill-conditioned') 
-        _Cinv    = np.linalg.inv(_Cpk) 
-        B = KLcompressor(_Cinv, dpkdt)
+        if method == 'KL':
+            cmpsr = Comp.Compressor(method='KL')   # KL compression 
+            cmpsr.fit(pks[:Nmock], dpkdt) # fit compression matrix
+        elif method == 'PCA': # PCA compression
+            cmpsr = Comp.Compressor(method='PCA')      
+            cmpsr.fit(pks[:Nmock], n_components=20, whiten=False)   
+        _cpks = cmpsr.transform(pks[:Nmock])
+        cpks = cmpsr.transform(pks)               # compressed P 
+        dcpkdt = cmpsr.transform(dpkdt)           # compressed dP/dtheta
 
-        # compressed P0
-        cpks    = np.dot(B, pks[:,klim].T).T
         cCpk    = np.cov(cpks.T) 
+        cCinv   = np.linalg.inv(cCpk) 
 
         ndata       = cCpk.shape[0]
         nmock       = cpks.shape[0]
         f_hartlap   = float(nmock - ndata - 2)/float(nmock - 1) 
-        cCinv       = f_hartlap * np.linalg.inv(cCpk) 
-        cdpkdt      = np.dot(B, dpkdt.T) 
+        cCinv       *= f_hartlap
         
-        # fisher 
-        cFij    = Forecast.Fij(cdpkdt, cCinv) 
-        # invert fisher matrix 
-        cFinv   = np.linalg.inv(cFij) 
-        print('cP0 sigmas %s' % ', '.join(['%.2e' % sii for sii in np.sqrt(np.diag(cFinv))]))
+        cFij    = Forecast.Fij(dcpkdt, cCinv) # fisher 
+        cFinv   = np.linalg.inv(cFij) # invert fisher matrix 
+
+        print('Nmock=%i; cP0 sigmas %s' % (Nmock, ', '.join(['%.2e' % sii for sii in np.sqrt(np.diag(cFinv))]))) 
         Finvs.append(cFinv) 
 
     theta_lbls = [r'$\Omega_m$', r'$\Omega_b$', r'$h$', r'$n_s$', r'$\sigma_8$', r'$M_\nu$']
@@ -134,52 +134,120 @@ def Pk_compression(kmax=0.5):
     bkgd = fig.add_subplot(111, frameon=False)
 
     for i, Nmock in enumerate(Nmocks[::-1]):
-        bkgd.fill_between([],[],[], color='C%i' % (i+1), label=r'KL comp. w/ $N_{\rm mock} = %i$' % Nmock) 
+        bkgd.fill_between([],[],[], color='C%i' % (i+1), 
+                label=r'%s comp. w/ $N_{\rm mock} = %i$' % (method.replace('_', ' '), Nmock))
     bkgd.fill_between([],[],[], color='C0', label=r'$P_0(k)$') 
     bkgd.legend(loc='upper right', bbox_to_anchor=(0.95, 0.95), fontsize=15)
     bkgd.text(0.8, 0.61, r'$k_{\rm max} = %.1f$; $z=0.$' % kmax, ha='right', va='bottom', transform=bkgd.transAxes, fontsize=15)
     bkgd.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
     fig.subplots_adjust(wspace=0.05, hspace=0.05) 
-    ffig = os.path.join(dir_fig, 'pkcompress.png') 
+    ffig = os.path.join(dir_fig, 'pkcompress.%s.png' % method) 
     fig.savefig(ffig, bbox_inches='tight') 
     return None 
 
 
-def PCAcompressor(X):
-    ''' calculate PCA compression matrix
+def Pk_compression(method='KL', kmax=0.5):
+    ''' Comparison of compressed P0 versus full P0
     '''
-    pass
-
-
-def KLcompressor(Cinv, dxdt): 
-    ''' calculate KL/MOPED compression matrix. For our purposes of Gaussian 
-    likelihood, this is the same as score compression 
-
-    :param Cinv 
-        (Ndim x Ndim) numpy array of data vectors. inverse covariance 
-        matrix
-
-    :param dxdt: 
-        (Ntheta x Ndim) numpy array of the derivative d<x>/dtheta. 
-
-    :return B: 
-        (Ntheta x Ndim) compression matrix  
-
-    references:
-        * Tegmark et al. (1997) 
-        * Heavens et al. (2000) 
-        * Gualdi et al. (2018) 
-        * Alsing et al. (2019) 
-    '''
-    assert Cinv.shape[1] == dxdt.shape[1]
-    ntheta = dxdt.shape[0] # number of parameters 
+    # read in Quijote P0
+    quij    = Obvs.quijotePk('fiducial', rsd=0, flag='reg') 
+    # k limit 
+    klim = (quij['k'] <= kmax) 
+    # calculate P0 covariance
+    pks     = quij['p0k'][:,klim] + quij['p_sn'][:,None] # shotnoise uncorrected P(k)
+    Cpk     = np.cov(pks.T) 
+    Cinv    = np.linalg.inv(Cpk) 
+    if np.linalg.cond(Cpk) >= 1e16: print('Covariance matrix is ill-conditioned') 
+    ndata       = Cpk.shape[0]
+    nmock       = pks.shape[0]
+    f_hartlap   = float(nmock - ndata - 2)/float(nmock - 1) 
+    Cinv        *= f_hartlap
     
-    B = [] 
-    for itheta in range(ntheta): 
-        dxdt_i = dxdt[itheta,:]
-        B_i = np.dot(Cinv, dxdt_i)  
-        B.append(B_i)  
-    return np.array(B) 
+    # derivative of P w.r.t theta
+    dpkdt = [] 
+    for par in ['Om', 'Ob2', 'h', 'ns', 's8', 'Mnu']: 
+        dpkdt_i = dPkdtheta(par, rsd='all', flag='reg', dmnu='fin')
+        dpkdt.append(dpkdt_i[klim])
+    dpkdt = np.array(dpkdt) 
+    
+    # true fisher 
+    Fij     = Forecast.Fij(dpkdt, Cinv) 
+    # invert fisher matrix 
+    Finv    = np.linalg.inv(Fij) 
+    print('P0 sigmas %s' % ', '.join(['%.2e' % sii for sii in np.sqrt(np.diag(Finv))]))
+
+    Finvs = []
+    Nmocks = [100, 250, 500, 1000, 5000][::-1]
+    for Nmock in Nmocks: 
+        if method == 'KL':
+            cmpsr = Comp.Compressor(method='KL')   # KL compression 
+            cmpsr.fit(pks[:Nmock], dpkdt) # fit compression matrix
+        elif method == 'PCA': # PCA compression
+            cmpsr = Comp.Compressor(method='PCA')      
+            cmpsr.fit(pks[:Nmock], n_components=20, whiten=False)   
+        _cpks = cmpsr.transform(pks[:Nmock])
+        cpks = cmpsr.transform(pks)               # compressed P 
+        dcpkdt = cmpsr.transform(dpkdt)           # compressed dP/dtheta
+
+        cCpk    = np.cov(cpks.T) 
+        cCinv   = np.linalg.inv(cCpk) 
+
+        ndata       = cCpk.shape[0]
+        nmock       = cpks.shape[0]
+        f_hartlap   = float(nmock - ndata - 2)/float(nmock - 1) 
+        cCinv       *= f_hartlap
+        
+        cFij    = Forecast.Fij(dcpkdt, cCinv) # fisher 
+        cFinv   = np.linalg.inv(cFij) # invert fisher matrix 
+
+        print('Nmock=%i; cP0 sigmas %s' % (Nmock, ', '.join(['%.2e' % sii for sii in np.sqrt(np.diag(cFinv))]))) 
+        Finvs.append(cFinv) 
+
+    theta_lbls = [r'$\Omega_m$', r'$\Omega_b$', r'$h$', r'$n_s$', r'$\sigma_8$', r'$M_\nu$']
+    theta_fid = [0.3175, 0.049, 0.6711, 0.9624, 0.834, 0.] # fiducial theta 
+    theta_lims = [(0.25, 0.385), (0.02, 0.08), (0.3, 1.1), (0.6, 1.3), (0.77, 0.9), (-0.4, 0.4)]
+    ntheta = len(theta_fid)
+
+    fig = plt.figure(figsize=(10, 10))
+    for i in range(ntheta): 
+        for j in range(i+1, ntheta): 
+            sub = fig.add_subplot(ntheta-1,ntheta-1,(ntheta-1)*(j-1)+i+1)
+            
+            theta_fid_i, theta_fid_j = theta_fid[i], theta_fid[j] # fiducial parameter 
+            for _i, _Finv in enumerate(Finvs[::-1]):
+                Finv_sub = np.array([[_Finv[i,i], _Finv[i,j]], [_Finv[j,i], _Finv[j,j]]]) # sub inverse fisher matrix 
+                Forecast.plotEllipse(Finv_sub, sub, theta_fid_ij=[theta_fid_i, theta_fid_j], color='C%i' % (_i+1))
+            Finv_sub = np.array([[Finv[i,i], Finv[i,j]], [Finv[j,i], Finv[j,j]]]) # sub inverse fisher matrix 
+            Forecast.plotEllipse(Finv_sub, sub, theta_fid_ij=[theta_fid_i, theta_fid_j], color='C0')
+
+            sub.set_xlim(theta_lims[i])
+            sub.set_ylim(theta_lims[j])
+            if i == 0:   
+                sub.set_ylabel(theta_lbls[j], labelpad=5, fontsize=28) 
+                sub.get_yaxis().set_label_coords(-0.35,0.5)
+            else: 
+                sub.set_yticks([])
+                sub.set_yticklabels([])
+            
+            if j == ntheta-1: 
+                sub.set_xlabel(theta_lbls[i], fontsize=26) 
+            else: 
+                sub.set_xticks([])
+                sub.set_xticklabels([]) 
+
+    bkgd = fig.add_subplot(111, frameon=False)
+
+    for i, Nmock in enumerate(Nmocks[::-1]):
+        bkgd.fill_between([],[],[], color='C%i' % (i+1), 
+                label=r'%s comp. w/ $N_{\rm mock} = %i$' % (method.replace('_', ' '), Nmock))
+    bkgd.fill_between([],[],[], color='C0', label=r'$P_0(k)$') 
+    bkgd.legend(loc='upper right', bbox_to_anchor=(0.95, 0.95), fontsize=15)
+    bkgd.text(0.8, 0.61, r'$k_{\rm max} = %.1f$; $z=0.$' % kmax, ha='right', va='bottom', transform=bkgd.transAxes, fontsize=15)
+    bkgd.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+    fig.subplots_adjust(wspace=0.05, hspace=0.05) 
+    ffig = os.path.join(dir_fig, 'pkcompress.%s.png' % method) 
+    fig.savefig(ffig, bbox_inches='tight') 
+    return None 
 
 
 def dPkdtheta(theta, log=False, rsd='all', flag='reg', dmnu='fin', returnks=False, silent=True):
@@ -217,4 +285,5 @@ def _flag_str(flag):
 
 
 if __name__=='__main__': 
-    Pk_compression()
+    #Pk_compression(method='KL')
+    Pk_compression(method='PCA')
---
+++ emanu/compressor.py
@@ -0,0 +1,123 @@
+'''
+'''
+import numpy as np 
+from sklearn.decomposition import PCA
+
+
+class Compressor(object): 
+    '''
+    '''
+    def __init__(self, method=None): 
+        '''
+        '''
+        if method not in ['PCA', 'KL']: 
+            raise NotImplementedError
+        self.method = method 
+        self.Xbar = None # average data vector
+        self.Xscale = None # component wise variance
+        self.cM = None # compression matrix
+
+    def fit(self, X, *args, **kwargs):  
+        ''' fit the compression 
+        
+        Parameters 
+        ----------
+        X : array 
+            (N x Ndim) array to fit the compression 
+        mean_sub : bool
+            If True, subtract mean from data vector before fitting compression  
+        '''
+        if self.method == 'KL': 
+            self._KL_fit(X, *args, **kwargs) 
+        elif self.method == 'PCA': 
+            self._PCA_fit(X, *args, **kwargs) 
+        else: 
+            raise NotImplementedError
+        return None 
+
+    def transform(self, Y): 
+        ''' transform (compress) input array using the compression
+        matrix calculated in `self.fit`
+
+        Parameters
+        ----------
+        Y : array
+            (N x Ndim) array that you want compressed. 
+
+        Returns 
+        -------
+        cY : array 
+            (N x Ncomp) compressed array 
+        '''
+        if self.method == 'PCA': 
+            cY = self._pca.transform(Y)
+        else: 
+            if self.cM is None: raise ValueError('first fit the compression') 
+            assert self.cM.shape[1] == Y.shape[1] 
+        
+            cY = np.dot(self.cM, Y.T).T 
+        return cY           
+
+    def _KL_fit(self, X, dxdt):
+        ''' fit KL/MOPED compression matrix. For our purposes of Gaussian 
+        likelihood, this is the same as score compression 
+    
+        Parameters 
+        ----------
+        X : array 
+            (N, Ndim) numpy array of data vector. These will be used to 
+            calculate the covariance and precision matricies 
+        dxdt : array 
+            (Ntheta x Ndim) numpy array of the derivative w.r.t. theta
+            d<x>/dtheta. 
+        
+        Returns
+        -------
+        cX : array 
+            (N x Ntheta) numpy array of compressed data vector
+        dcXdt : array
+            (Ntheta x Ntheta) numpy array of the compressed derivative 
+
+        References
+        ----------
+        * Tegmark et al. (1997) 
+        * Heavens et al. (2000) 
+        * Gualdi et al. (2018) 
+        * Alsing et al. (2019) 
+        '''
+        assert X.shape[1] == dxdt.shape[1]
+        ntheta = dxdt.shape[0] # number of parameters 
+
+        Cx = np.cov(X.T) 
+        if np.linalg.cond(Cx) >= 1e16: print('Covariance matrix is ill-conditioned') 
+        iCx = np.linalg.inv(Cx) 
+        
+        B = np.zeros((ntheta, X.shape[1]))  
+        for itheta in range(ntheta): 
+            dxdt_i = dxdt[itheta]
+            B_i = np.dot(iCx, dxdt_i)  
+            B[itheta,:] = B_i
+        self.cM = B.copy() 
+        return None 
+
+    def _PCA_fit(self, X, n_components=6, whiten=False): 
+        ''' fit PCA compression matrix. 
+
+        Parameters
+        ----------
+        X : array 
+            (N, Ndim) numpy array of data vector. 
+        n_components : int 
+            (default: 6) number of PCA components 
+        '''
+        #self.Xbar = np.mean(X, axis=0) 
+        #X -= self.Xbar
+        #Cx = np.cov(X.T)  
+        #print Cx.shape
+        #l, principal_axes = np.linalg.eig(Cx) 
+        #isort = l.argsort()[::-1]
+        #self.cM = principal_axes[:n_components,isort]
+        self._pca = PCA(copy=True, n_components=n_components, whiten=whiten)
+        self._pca.fit(X) 
+        self.cM = self._pca.components_
+        return None 
