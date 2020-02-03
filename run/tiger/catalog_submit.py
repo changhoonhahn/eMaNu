f6e16929f7b0ccd7ce68c5e8f43fdc975615452f
changhoonhahn <changhoonhahn@lbl.gov>
2020-02-03 10:28:19 -0800

    scripts for constructing hodcatalogs

---
+++ run/tiger/catalog_submit.py
@@ -0,0 +1,81 @@
+# This script is used to submit jobs to construct HOD catalogs 
+import numpy as np
+import sys,os
+
+################################## INPUT #############################################
+step       = 500  #number of realizations each cpu will do
+offset     = 0    #the count will start from offset
+snapnum    = 4    #4(z=0), 3(z=0.5), 2(z=1), 1(z=2), 0(z=3)
+######################################################################################
+
+thetas = ['Om_p', 'Ob_p', 'Ob2_p', 'h_p', 'ns_p', 's8_p', 'Om_m', 'Ob_m', 'Ob2_m', 'h_m', 'ns_m', 's8_m', 
+        'Mnu_p', 'Mnu_pp', 'Mnu_ppp', 'logMmin_m', 'logMmin_p', 'sigma_logM_m', 'sigma_logM_p', 'logM0_m', 
+        'logM0_p', 'alpha_m', 'alpha_p', 'logM1_m', 'logM1_p', 'fiducial', 'fiducial_ZA']
+
+# do a loop over the different cosmologies
+for theta in thetas: 
+
+    folder = theta 
+    if theta == 'testing': 
+        nodes   = 1 
+        folder  = 'fiducial'
+        step    = 1 
+    elif theta == 'fiducial':            
+        nodes = 15000/step
+    elif theta =='latin_hypercube':    
+        nodes = 2000/step
+    else:                           
+        nodes = 500/step
+        if theta in ['logMmin_m', 'logMmin_p', 'sigma_logM_m', 'sigma_logM_p', 'logM0_m', 
+                'logM0_p', 'alpha_m', 'alpha_p', 'logM1_m', 'logM1_p']: 
+            folder = 'fiducial'
+    
+    logMmin    = 13.65
+    sigma_logM = 0.20
+    logM0      = 14.0
+    alpha      = 1.1
+    logM1      = 14.0      
+    if theta == 'logMmin_m': 
+        logMmin = 13.6
+    elif theta == 'logMmin_p': 
+        logMmin = 13.7
+    elif theta == 'sigma_logM_m': 
+        sigma_logM = 0.18
+    elif theta == 'sigma_logM_p': 
+        sigma_logM = 0.22
+    elif theta == 'logM0_m': 
+        logM0_m = 13.8
+    elif theta == 'logM0_p': 
+        logM0_p = 14.2
+    elif theta == 'alpha_m': 
+        alpha = 0.9
+    elif theta == 'alpha_p': 
+        alpha = 1.3
+    elif theta == 'logM1_m': 
+        logM1 = 13.8
+    elif theta == 'logM1_p': 
+        logM1 = 14.2
+
+    for i in range(nodes): # loop over the different realizations
+        a = '\n'.join(["#!/bin/bash", 
+            "#SBATCH -J HOD",
+            "#SBATCH --exclusive",
+            "#SBATCH --nodes=1",
+            "#SBATCH --ntasks-per-node=48",
+            "#SBATCH --partition=general",
+            "#SBATCH --export=ALL",
+            "#SBATCH --mail-type=all",
+            "#SBATCH --mail-user=changhoonhahn@lbl.gov",
+            "", 
+            "module load anaconda3", 
+            "source activate emanu", 
+            "",
+            "srun -n 1 --mpi=pmi2 python3 create_hodcatalog.py %d %d %s %d %s %s %s %s %s" % (i*step+offset, (i+1)*step+offset, folder, snapnum, logMmin, sigma_logM, logM0, alpha, logM1),
+            ""]) 
+
+        # create the script.sh file, execute it and remove it
+        f = open('script.slurm','w')
+        f.write(a)
+        f.close()
+        os.system('sbatch script.slurm')
+        os.system('rm script.slurm')
---
+++ run/tiger/create_hodcatalog.py
@@ -0,0 +1,157 @@
+# This script computes the galaxy bispectrum in real- and redshift-space. It takes as
+# input the first and last number of the wanted realizations, the cosmology and the 
+# snapnum. In redshift-space it computes the bispectrum along the 3 different axes. 
+import argparse
+from mpi4py import MPI
+import numpy as np
+import sys,os,h5py
+from emanu import forwardmodel as FM
+from emanu.sims import data as simData
+from pyspectrum import pyspectrum as pySpec
+
+###### MPI DEFINITIONS ######                                    
+comm   = MPI.COMM_WORLD
+nprocs = comm.Get_size()
+myrank = comm.Get_rank()
+
+# read the first and last realization to identify voids
+parser = argparse.ArgumentParser(description="This script constructs the HOD galaxy catalog")
+parser.add_argument("first",      help="first realization number", type=int)
+parser.add_argument("last",       help="last  realization number", type=int)
+parser.add_argument("cosmo",      help="folder with the realizations")
+parser.add_argument("snapnum",    help="snapshot number",          type=int)
+parser.add_argument("logMmin",    help="logMmin",                  type=float)
+parser.add_argument("sigma_logM", help="sigma_logM",               type=float)
+parser.add_argument("logM0",      help="logM0",                    type=float)
+parser.add_argument("alpha",      help="alpha",                    type=float)
+parser.add_argument("logM1",      help="logM1",                    type=float)
+args = parser.parse_args()
+first, last, cosmo, snapnum = args.first, args.last, args.cosmo, args.snapnum
+logMmin, sigma_logM = args.logMmin, args.sigma_logM
+logM0, alpha, logM1 = args.logM0, args.alpha, args.logM1
+
+def create_HOD(halo_folder, snap_folder, snapnum, hod_dict, seed, fGC):
+    ''' Compute and saves the galaxy catalog to hdf5 
+    '''
+    # read in the halo catalog
+    halos = simData.hqHalos(halo_folder, snap_folder, snapnum) 
+
+    # populate halos with galaxies
+    hod = FM.hodGalaxies(halos, hod_dict, seed=seed)
+    
+    # get positions and velocities of the galaxies
+    pos = np.array(hod['Position'])
+    vel = np.array(hod['Velocity'])
+
+    # extra columns (for Christina and Alice) 
+    # halo position 
+    x_h = np.array(hod['halo_x']) 
+    y_h = np.array(hod['halo_y']) 
+    z_h = np.array(hod['halo_z']) 
+    pos_halo = np.array([x_h, y_h, z_h]).T
+
+    # halo velocity 
+    vx_h = np.array(hod['halo_vx']) 
+    vy_h = np.array(hod['halo_vy']) 
+    vz_h = np.array(hod['halo_vz']) 
+    vel_halo = np.array([vx_h, vy_h, vz_h]).T
+    
+    # halo virial mass 
+    mvir_halo = np.array(hod['halo_mvir']) 
+    # halo virial radius
+    rvir_halo = np.array(hod['halo_rvir']) 
+    # halo id 
+    id_halo = np.array(hod['halo_id']) 
+    # gal_type (0:central, 1:satellite) 
+    gal_type = np.array(hod['gal_type'])
+
+    # save catalogue to file
+    f = h5py.File(fhod, 'w')
+    f.create_dataset('pos', data=pos)
+    f.create_dataset('vel', data=vel)
+    f.create_dataset('halo_pos', data=pos_halo) 
+    f.create_dataset('halo_vel', data=vel_halo) 
+    f.create_dataset('halo_mvir', data=mvir_halo) 
+    f.create_dataset('halo_rvir', data=rvir_halo) 
+    f.create_dataset('halo_id', data=id_halo) 
+    f.create_dataset('gal_type', data=gal_type) 
+    f.close()
+    return None 
+
+##################################### INPUT #########################################
+# general parameters
+root    = '/projects/QUIJOTE'
+snapnum = 4 #4(z=0), 3(z=0.5), 2(z=1), 1(z=2), 0(z=3)
+
+# HOD parameter values
+seed     = 0    #random seed
+hod_dict = {
+    'logMmin': logMmin, 
+    'sigma_logM': sigma_logM, 
+    'logM0': logM0, 
+    'alpha': alpha, 
+    'logM1': logM1}
+
+# output folder name
+root_out = '/projects/QUIJOTE/Galaxies/' 
+#####################################################################################
+
+# find the redshift
+z = {4:0, 3:0.5, 2:1, 1:2, 0:3}[snapnum]
+
+suffix = None
+if logMmin!=13.65:   suffix='logMmin=%.2f' % logMmin
+if sigma_logM!=0.2:  suffix='sigma_logM=%.2f' % sigma_logM
+if logM0!=14.0:      suffix='logM0=%.1f' % logM0
+if alpha!=1.1:       suffix='alpha=%.1f' % alpha
+if logM1!=14.0:      suffix='logM1=%.1f' % logM1
+
+if suffix!=None:  cosmo2 = '%s_%s'%(cosmo, suffix)
+else:             cosmo2 = cosmo
+
+# create output folder if it does not exist
+if myrank==0:
+    if not os.path.exists(root_out+cosmo2):  os.system('mkdir %s/%s/'%(root_out,cosmo2))
+
+# get the realizations each cpu works on
+numbers = np.where(np.arange(args.first, args.last)%nprocs==myrank)[0]
+numbers = np.arange(args.first, args.last)[numbers]
+
+######## standard simulations #########
+for i in numbers:
+    print(i)
+
+    # find halo and snap folders
+    halo_folder = '%s/Halos/%s/%d' % (root,cosmo,i)
+    snap_folder = '%s/Snapshots/%s/%d' % (root,cosmo,i)
+    if not os.path.exists(snap_folder):
+        continue
+
+    # find output folder and create it if it doesnt exist
+    folder_out = '%s/%s/%d/' % (root_out, cosmo2, i)
+    if not os.path.exists(folder_out):  
+        os.system('mkdir %s'%folder_out)
+
+    # create galaxy catalogue
+    fGC = '%s/GC_%d_z=%s.hdf5' % (folder_out, seed, z)
+    if not os.path.exists(fGC):  
+        create_HOD(halo_folder, snap_folder, snapnum, hod_dict, seed, fGC)
+
+###### paired fixed realizations ######
+for i in numbers:
+
+    for pair in [0,1]:
+
+        # find halo and snap folders
+        halo_folder = '%s/Halos/%s/NCV_%d_%d'%(root,cosmo,pair,i)
+        snap_folder = '%s/Snapshots/%s/NCV_%d_%d'%(root,cosmo,pair,i)
+        if not(os.path.exists(snap_folder)):  continue
+
+        # find output folder and create it if it doesnt exist
+        folder_out = '%s/%s/NCV_%d_%d/'%(root_out, cosmo2, pair, i)
+        if not(os.path.exists(folder_out)):  os.system('mkdir %s'%folder_out)
+
+        # create galaxy catalogue
+        fGC = '%s/GC_%d_z=%s.hdf5'%(folder_out,seed,z)
+        if not(os.path.exists(fGC)):  
+            create_HOD(halo_folder, snap_folder, snapnum, hod_dict, seed, fGC)
