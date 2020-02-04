49a80fea6c5b9b141d4a90cc0cede98499f0311c
chang.nersc <changh20@gmail.com>
2020-02-03 17:27:24 -0800

    quijote header lookup table included

--- run/cori/check_headers.py
+++ run/cori/check_headers.py
@@ -1,10 +1,15 @@
+import os 
+from emanu import util as UT 
 from emanu.sims import readgadget as RG
 
-thetas = ['Om_p', 'Ob_p', 'Ob2_p', 'h_p', 'ns_p', 's8_p', 'Om_m', 'Ob_m', 'Ob2_m', 'h_m', 'ns_m', 's8_m',
+thetas = ['Om_p', 'Ob2_p', 'h_p', 'ns_p', 's8_p', 'Om_m', 'Ob2_m', 'h_m', 'ns_m', 's8_m',
         'Mnu_p', 'Mnu_pp', 'Mnu_ppp', 'fiducial', 'fiducial_ZA']
 
 snapnum = 4 
-for theta in thetas 
+
+f = open(os.path.join(UT.code_dir(), 'emanu', 'dat', 'quijote_header_lookup.dat'), 'w')
+f.write('# theta snapnum Om Ol z h Hz\n')
+for theta in thetas:
     snap_folder = os.path.join(UT.dat_dir(), 'quijote', 'Snapshots', theta, '0')
     # read in Gadget header (~65.1 microsec) 
     header = RG.header(os.path.join(snap_folder, 'snapdir_%s' % str(snapnum).zfill(3), 
@@ -14,4 +19,5 @@ for theta in thetas
     z   = header.redshift
     h   = header.hubble 
     Hz  = header.Hubble #100.0 * np.sqrt(Om * (1.0 + z)**3 + Ol) # km/s/(Mpc/h)
-    print(Om, Ol, z, h, Hz) 
+    f.write('%s\n' % '\t'.join([str(tt) for tt in [theta, snapnum, Om, Ol, z, h, Hz]])) 
+f.close() 
