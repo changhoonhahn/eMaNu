2f23a0c15cc50063e6475c97e8470d5259b70cf1
changhoonhahn <changhoonhahn@lbl.gov>
2020-04-30 11:26:31 -0700

    wp and xi forecasts for Jeremy

--- run/wpxi_forecast.py
+++ run/wpxi_forecast.py
@@ -7,6 +7,7 @@ hod catalogs that Jeremy made
 import os 
 import h5py 
 import numpy as np 
+from emanu import forecast as Forecast
 # --- plotting --- 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -28,20 +29,131 @@ else:
     raise ValueError
 
 
+thetas = ['Om', 'Ob2', 'h', 'ns', 's8', 'Mnu', 'logMmin', 'sigma_logM', 'alpha', 'logM1'] 
+
+theta_lbls = [r'$\Omega_m$', r'$\Omega_b$', '$h$', '$n_s$', r'$\sigma_8$', r'$M_\nu$', 
+        r'$\log M_{\rm min}$', r'$\sigma_{\log M}$', r'$\alpha$', r'$\log M_1$'] 
+
+theta_fid = {'Mnu': 0., 'Ob': 0.049, 'Ob2': 0.049, 'Om': 0.3175, 'h': 0.6711,  'ns': 0.9624,  's8': 0.834, 
+        'logMmin': 13.65, 'sigma_logM': 0.2, 'logM0': 14.0, 'alpha': 1.1, 'logM1': 14.0} # fiducial theta 
+
+
+def forecast(obs):
+    ''' fisher forecast  
+    '''
+    Fij = FisherMatrix(obs) # fisher matrix (Fij)
+
+    cond = np.linalg.cond(Fij)
+    if cond > 1e16: print('Fij is ill-conditioned %.5e' % cond)
+
+    Finv    = np.linalg.inv(Fij) # invert fisher matrix 
+    print('sigmas %s' % ', '.join(['%.2e' % sii for sii in np.sqrt(np.diag(Finv))]))
+
+    # x, y ranges
+    dlim = [0.1, 0.06, 0.5, 0.5, 0.15, 0.75, 0.2, 0.4, 0.2, 0.2]
+
+    fig = plt.figure(figsize=(17, 15))
+    for i in range(len(thetas)+1): 
+        for j in range(i+1, len(thetas)): 
+            # sub inverse fisher matrix 
+            Finv_sub = np.array([[Finv[i,i], Finv[i,j]], [Finv[j,i], Finv[j,j]]]) 
+            # plot the ellipse
+            sub = fig.add_subplot(len(thetas)-1, len(thetas)-1, (len(thetas)-1) * (j-1) + i + 1) 
+
+            Forecast.plotEllipse(Finv_sub, sub, 
+                    theta_fid_ij=[theta_fid[thetas[i]], theta_fid[thetas[j]]], color='C0')
+
+            sub.set_xlim(theta_fid[thetas[i]] - dlim[i], theta_fid[thetas[i]] + dlim[i])
+            sub.set_ylim(theta_fid[thetas[j]] - dlim[j], theta_fid[thetas[j]] + dlim[j])
+            if i == 0:   
+                sub.set_ylabel(theta_lbls[j], fontsize=20) 
+            else: 
+                sub.set_yticks([])
+                sub.set_yticklabels([])
+            
+            if j == len(thetas)-1: 
+                sub.set_xlabel(theta_lbls[i], labelpad=10, fontsize=20) 
+            else: 
+                sub.set_xticks([])
+                sub.set_xticklabels([]) 
+
+    fig.subplots_adjust(wspace=0.05, hspace=0.05) 
+    ffig = os.path.join(dat_dir, '%s.forecast.png' % obs)
+    fig.savefig(ffig, bbox_inches='tight') 
+    return None 
+
+
+def plot_FisherMatrix(obs): 
+    ''' plot the Fisher matrix
+    '''
+    Fij = FisherMatrix(obs)
+
+    theta_lbls = [r'$\Omega_m$', r'$\Omega_b$', '$h$', '$n_s$', r'$\sigma_8$',
+            r'$\log M_{\rm min}$', r'$\sigma_{\log M}$', r'$\alpha$', 
+            r'$\log M_1$'] 
+    
+    # plot the  fisher matrix 
+    fig = plt.figure(figsize=(20,7))
+    sub = fig.add_subplot(121)
+    cm = sub.pcolormesh(Fij)#, vmin=-0.025, vmax=1.) 
+    cbar = fig.colorbar(cm, ax=sub) 
+
+    sub.set_xticks(np.arange(len(theta_lbls))+0.5) 
+    sub.set_xticklabels(theta_lbls) 
+    sub.set_yticks(np.arange(len(theta_lbls))+0.5) 
+    sub.set_yticklabels(theta_lbls) 
+    
+    if obs == 'wp': 
+        obs_lbl = '$w_p$'
+    elif obs == 'xi': 
+        obs_lbl = r'$\xi_0, \xi_2$'
+    cbar.set_label(r'%s fisher matrix' % obs_lbl, fontsize=25, labelpad=10, rotation=90)
+
+    ffig = os.path.join(dat_dir, 'fij_%s.png' % obs)
+    fig.savefig(ffig, bbox_inches='tight') 
+    return None 
+
+
+# Fisher Matrix
+def FisherMatrix(obs): 
+    ''' calculate fisher matrix 
+    '''
+    # calculate covariance matrix (with shotnoise; this is the correct one) 
+    # *** rsd and flag kwargs are ignored for the covariace matirx ***
+    if obs == 'wp': 
+        C_fid, nmock = cov_wp()
+        dobsdt = dwpdtheta
+    elif obs == 'xi': 
+        C_fid, nmock = cov_xi()
+        dobsdt = dxidtheta
+    else: 
+        raise NotImplementedError
+    print('covariance matrix from %i mocks' % nmock)
+    print('     condition number %e' % np.linalg.cond(C_fid))
+    
+    ndata = C_fid.shape[0] 
+    f_hartlap = float(nmock - ndata - 2)/float(nmock - 1) 
+    C_inv = f_hartlap * np.linalg.inv(C_fid) # invert the covariance 
+    
+    # calculate the derivatives along all the thetas 
+
+    dobs_dt = [] 
+    for par in thetas: 
+        _, dobsdt_i = dobsdt(par)
+        dobs_dt.append(dobsdt_i) 
+            
+    Fij = Forecast.Fij(dobs_dt, C_inv) 
+    return Fij 
+
+
 def plot_dwpxi(): 
     ''' compare wp, xi0, and xi2 derivatives w.r.t. the different thetas
     '''
-    #thetas = ['Om', 'Ob2', 'h', 'ns', 's8', 'Mnu', 'logMmin', 'sigma_logM', 'logM0', 'alpha', 'logM1'] 
-    #theta_lbls = [r'$\Omega_m$', r'$\Omega_b$', r'$h$', r'$n_s$', r'$\sigma_8$', r'$M_\nu$', 
-    #        r'$\log M_{\rm min}$', r'$\sigma_{\log M}$', r'$\log M_0$', r'$\alpha$', r'$\log M_1$']
-    thetas = ['Om', 'Ob2', 'h', 'ns', 's8', 'logMmin', 'sigma_logM', 'alpha', 'logM1'] 
-    theta_lbls = [r'$\Omega_m$', r'$\Omega_b$', r'$h$', r'$n_s$', r'$\sigma_8$', 
-            r'$\log M_{\rm min}$', r'$\sigma_{\log M}$', r'$\alpha$', r'$\log M_1$']
     # y range of P0, P2 plots 
     #logplims = [(-10., 5.), (-2, 15), (-3., 1.), (-4., 0.), (-2., 2.), (-0.5, 0.5), (-2., 4), (-2., 1.), None, (-1., 2.), (-5., 1.)] 
     #logblims = [(-13., -6.), (-2., 24.), (-5., -0.5), (-5., -0.5), (-2., 2.), (0.2, 0.7), (3., 6.), None, None, (2, 6), None] 
 
-    fig = plt.figure(figsize=(12,3*len(thetas)))
+    fig = plt.figure(figsize=(15,3*len(thetas)))
     for i, theta in enumerate(thetas): 
         r, dwp = dwpdtheta(theta)
         
@@ -55,7 +167,9 @@ def plot_dwpxi():
         #sub.set_ylim(logplims[i]) 
         if i == 4: sub.set_ylabel(r'${\rm d} w_{\rm p}/{\rm d}\theta$', fontsize=25) 
         
-        r, dxi0, dxi2 = dxidtheta(theta)
+        r, dxis = dxidtheta(theta)
+        dxi0 = dxis[:len(dxis)//2]
+        dxi2 = dxis[len(dxis)//2:]
 
         # plot dB/dtheta
         sub = fig.add_subplot(len(thetas), 3, 3*i+2)
@@ -82,6 +196,7 @@ def plot_dwpxi():
         if i != len(thetas)-1: sub.set_xticklabels([]) 
 
     ffig = os.path.join(dat_dir, 'dwpxidtheta.png')
+    fig.subplots_adjust(wspace=0.5, hspace=0.02) 
     fig.savefig(ffig, bbox_inches='tight') 
     return None 
 
@@ -89,8 +204,10 @@ def plot_dwpxi():
 def plot_cov(): 
     ''' compare covariance of wp, xi0, and xi2
     '''
-    C_wp = cov_wp()
-    C_xi = cov_xi()
+    C_wp, _ = cov_wp()
+    C_xi, _ = cov_xi()
+    print('condition number for C_wp: %e' % np.linalg.cond(C_wp))
+    print('condition number for C_xi: %e' % np.linalg.cond(C_xi))
     
     Ccorr_wp = ((C_wp / np.sqrt(np.diag(C_wp))).T / np.sqrt(np.diag(C_wp))).T
     Ccorr_xi = ((C_xi / np.sqrt(np.diag(C_xi))).T / np.sqrt(np.diag(C_xi))).T
@@ -114,10 +231,11 @@ def plot_cov():
 def cov_wp(): 
     ''' calculate covariance of wp 
     '''
-    quij = quijhod_wp('fiducial')
-    wps = quij['wp']
-    cov = np.cov(wps.T) # calculate the covariance
-    return cov
+    quij    = quijhod_wp('fiducial')
+    wps     = quij['wp']
+    nmock   = wps.shape[0] 
+    cov     = np.cov(wps.T) # calculate the covariance
+    return cov, nmock
 
 
 def cov_xi(): 
@@ -129,8 +247,9 @@ def cov_xi():
     
     xis = np.concatenate([xi0, xi2], axis=1)
 
+    nmock = xis.shape[0]
     cov = np.cov(xis.T) # calculate the covariance
-    return cov
+    return cov, nmock
 
 
 def dwpdtheta(theta): 
@@ -151,8 +270,10 @@ def dwpdtheta(theta):
             'logM1': [13.8, 14.2]}
 
     if theta == 'Mnu': 
-        # don't think Jeremy calculated the ZA wps 
-        raise NotImplementedError
+        # this is wrong. It should fiducial_ZA 
+        steps = ['fiducial', 'Mnu_p', 'Mnu_pp', 'Mnu_ppp'] 
+        coeffs = [-21., 32., -12., 1.] # finite difference coefficient
+        h = 1.2
     else: 
         steps = ['%s_%s' % (theta, step) for step in ['m', 'p']]
         coeffs = [-1, 1.]
@@ -191,8 +312,10 @@ def dxidtheta(theta):
             'logM1': [13.8, 14.2]}
 
     if theta == 'Mnu': 
-        # don't think Jeremy calculated the ZA wps 
-        raise NotImplementedError
+        # this is wrong. It should fiducial_ZA 
+        steps = ['fiducial', 'Mnu_p', 'Mnu_pp', 'Mnu_ppp'] 
+        coeffs = [-21., 32., -12., 1.] # finite difference coefficient
+        h = 1.2
     else: 
         steps = ['%s_%s' % (theta, step) for step in ['m', 'p']]
         coeffs = [-1, 1.]
@@ -213,7 +336,9 @@ def dxidtheta(theta):
     r       /= float(len(steps))
     dxi0    /= h 
     dxi2    /= h 
-    return r, dxi0, dxi2
+
+    dxis = np.concatenate([dxi0, dxi2])
+    return r, dxis
 
 
 def quijhod_wp(theta): 
@@ -253,7 +378,9 @@ def quijhod_xi(theta):
 
 
 if __name__=='__main__': 
+    #plot_cov()
     #plot_dwpxi()
-    #cov_wp()
-    #cov_xi()
-    plot_cov()
+    #plot_FisherMatrix('wp')
+    #plot_FisherMatrix('xi')
+    forecast('wp')
+    forecast('xi')
